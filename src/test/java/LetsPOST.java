f0a1fd2a4af733ebc37421115f290f272e66003f
Your Name <you@example.com>
2021-07-22 10:55:50 +0300

    fail

--- src/test/java/LetsPOST.java
+++ src/test/java/LetsPOST.java
@@ -17,7 +17,7 @@ public class LetsPOST {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
